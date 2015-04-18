3f56847b6e992677590c3612a587b00e783a86f8
Marcelo Mottalli <mottalli@yahoo.com>
2015-04-18 17:22:30 -0300

    Beautify

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,81 @@ mod llvm {
     use std::mem;
     use std::str;
 
+    /*************************************************************************/
     extern fn __morestack() {
         println!("REQUESTED MORE STACK! SHOULD NOT HAPPEN!");
     }
 
+    /*************************************************************************/
+    pub trait LLVMType {
+        fn to_llvm_type(&self) -> rustc_llvm::TypeRef;
+    }
+    
+    /*************************************************************************/
+    pub trait LLVMValue {
+        fn to_llvm_value(&self) -> rustc_llvm::ValueRef;
+        fn get_type(&self) -> Type; 
+
+        fn get_llvm_type(&self) -> rustc_llvm::TypeRef {
+            self.get_type().to_llvm_type()
+        }
+    }
+
+    /*************************************************************************/
     pub struct Module {
         _context: rustc_llvm::ContextRef,
         _module: rustc_llvm::ModuleRef
     }
 
+    /*************************************************************************/
+    pub struct Builder<'a> {
+        _module: &'a mut Module,
+        _builder: rustc_llvm::BuilderRef
+    }
+
+    /*************************************************************************/
+    pub struct Type {
+        _type: rustc_llvm::TypeRef
+    }
+
+    impl LLVMType for Type {
+        fn to_llvm_type(&self) -> rustc_llvm::TypeRef { self._type }
+    }
+
+    /*************************************************************************/
+    pub struct FunctionType {
+        _type: rustc_llvm::TypeRef
+    }
+
+    impl LLVMType for FunctionType {
+        fn to_llvm_type(&self) -> rustc_llvm::TypeRef { self._type }
+    }
+
+    /*************************************************************************/
+    pub struct Function {
+        _value: rustc_llvm::ValueRef
+    }
+
+    /*************************************************************************/
+    pub struct BasicBlock {
+        _block: rustc_llvm::BasicBlockRef
+    }
+
+    /*************************************************************************/
+    pub struct Value {
+        _value: rustc_llvm::ValueRef
+    }
+
+    /*************************************************************************/
+    pub struct ExecutionEngine {
+        _engine: rustc_llvm::ExecutionEngineRef,
+        _module: Module
+    }
+
+    /*************************************************************************/
     fn _get_string_ptr(s: &str) -> *const libc::c_char { CString::new(s).unwrap().as_ptr() }
 
+    /*************************************************************************/
     impl Module {
         pub fn new(name: &str) -> Module {
             unsafe {
@@ -36,36 +100,7 @@ mod llvm {
         }
     }
 
-    pub struct Builder<'a> {
-        _module: &'a mut Module,
-        _builder: rustc_llvm::BuilderRef
-    }
-
-    pub struct Type {
-        _type: rustc_llvm::TypeRef
-    }
-
-    pub struct FunctionType {
-        _type: rustc_llvm::TypeRef
-    }
-
-    pub struct Function {
-        _value: rustc_llvm::ValueRef
-    }
-
-    pub struct BasicBlock {
-        _block: rustc_llvm::BasicBlockRef
-    }
-
-    pub struct Value {
-        _value: rustc_llvm::ValueRef
-    }
-
-    pub struct ExecutionEngine {
-        _engine: rustc_llvm::ExecutionEngineRef
-    }
-
-
+    /*************************************************************************/
     impl<'a> Builder<'a> {
         pub fn new(module: &'a mut Module) -> Builder {
             let context = module._context;
@@ -76,11 +111,11 @@ mod llvm {
 
         pub fn get_int32_type(&mut self) -> Type { unsafe { Type{ _type: rustc_llvm::LLVMInt32TypeInContext(self._module._context) } } }
 
-        pub fn create_function_type(&mut self, return_type: &Type, param_types: &[Type]) -> FunctionType {
-            let llvm_types: Vec<rustc_llvm::TypeRef> = param_types.iter().map(|t| t._type).collect();
+        pub fn create_function_type(&mut self, return_type: &LLVMType, param_types: &[&LLVMType]) -> FunctionType {
+            let llvm_types: Vec<rustc_llvm::TypeRef> = param_types.iter().map(|t| t.to_llvm_type()).collect();
             let p_types: *const rustc_llvm::TypeRef = llvm_types.as_ptr();
             unsafe {
-                FunctionType{ _type: rustc_llvm::LLVMFunctionType(return_type._type, p_types, param_types.len() as libc::c_uint, rustc_llvm::False) }
+                FunctionType{ _type: rustc_llvm::LLVMFunctionType(return_type.to_llvm_type(), p_types, param_types.len() as libc::c_uint, rustc_llvm::False) }
             }
         }
         
@@ -119,12 +154,14 @@ mod llvm {
         }
     }
 
+    /*************************************************************************/
     impl ExecutionEngine {
-        pub fn new(module: &Module) -> ExecutionEngine {
+        pub fn new(module: Module) -> ExecutionEngine {
             unsafe {
                 let morestack: *const () = mem::transmute(__morestack);
                 let jit_memory_manager = rustc_llvm::LLVMRustCreateJITMemoryManager(morestack);
-                let engine = ExecutionEngine{ _engine: rustc_llvm::LLVMBuildExecutionEngine(module._module, jit_memory_manager) };
+                // Is this OK?
+                let engine = ExecutionEngine{ _engine: rustc_llvm::LLVMBuildExecutionEngine(module._module, jit_memory_manager), _module: module };
                 let c_error = rustc_llvm::LLVMRustGetLastError();
                 if !c_error.is_null() {
                     let error = CStr::from_ptr(c_error);
@@ -134,19 +171,17 @@ mod llvm {
             }
         }
 
-        pub fn finalize(&self) -> &ExecutionEngine {
+        pub fn finalize(&mut self) -> &mut ExecutionEngine {
             unsafe { rustc_llvm::LLVMExecutionEngineFinalizeObject(self._engine); self }
         }
 
         pub fn get_pointer_to_function(&self, function: &Function) -> *const() {
-            unsafe {
-                rustc_llvm::LLVMGetPointerToGlobal(self._engine, function._value)
-
-            }
+            unsafe { rustc_llvm::LLVMGetPointerToGlobal(self._engine, function._value) }
         }
     }
 }
 
+/*************************************************************************/
 fn main() {
     let mut module = llvm::Module::new("ModuloDePrueba");
     let mut function: llvm::Function;
@@ -156,25 +191,27 @@ fn main() {
         let return_type = builder.get_int32_type();
         let param1_type = builder.get_int32_type();
         let param2_type = builder.get_int32_type();
-        let function_type = builder.create_function_type(&return_type, &[param1_type, param2_type]);
+        let function_type = builder.create_function_type(&return_type, &[&param1_type, &param2_type]);
         function = builder.create_function("foo", &function_type);
         let init_block = builder.create_basic_block("init", &mut function);
-        builder.set_insert_point(&init_block);
-        let param1 = builder.get_param(&function, 0);
-        let param2 = builder.get_param(&function, 1);
-        let sum = builder.create_add(&param1, &param2);
-        builder.create_ret(&sum);
+        {
+            builder.set_insert_point(&init_block);
+            let param1 = builder.get_param(&function, 0);
+            let param2 = builder.get_param(&function, 1);
+            let sum = builder.create_add(&param1, &param2);
+            builder.create_ret(&sum);
+        }
     }
     module.print();
 
-    let mut engine = llvm::ExecutionEngine::new(&module);
+    let mut engine = llvm::ExecutionEngine::new(module);
     engine.finalize();
     let fptr: *const() = engine.get_pointer_to_function(&function);
     assert!(!fptr.is_null());
     unsafe {
         println!("Function pointer: {:?}", fptr);
         println!("Ejecutando...");
-        let f: fn(i32, i32) -> i32 = unsafe { mem::transmute(fptr) };
+        let f: fn(i32, i32) -> i32 = mem::transmute(fptr);
         let res = f(12, 13);
         println!("res: {}", res);
     }
