b024e9428686cd3e2714cd870b58baaabe128dba
Marcelo Mottalli <mottalli@yahoo.com>
2015-04-18 23:24:00 -0300

    Beautify

--- src/main.rs
+++ src/main.rs
@@ -38,8 +38,7 @@ mod llvm {
     }
 
     /*************************************************************************/
-    pub struct Builder<'a> {
-        _module: &'a mut Module,
+    pub struct Builder {
         _builder: rustc_llvm::BuilderRef
     }
 
@@ -63,12 +62,14 @@ mod llvm {
 
     /*************************************************************************/
     pub struct Function {
-        _value: rustc_llvm::ValueRef
+        _value: rustc_llvm::ValueRef,
+        _module: *mut Module
     }
 
     /*************************************************************************/
     pub struct BasicBlock {
-        _block: rustc_llvm::BasicBlockRef
+        _block: rustc_llvm::BasicBlockRef,
+        _function: *mut Function
     }
 
     /*************************************************************************/
@@ -95,17 +96,7 @@ mod llvm {
             }
         }
 
-        pub fn print(&self) { unsafe { rustc_llvm::LLVMDumpModule(self._module); } }
-    }
-
-    /*************************************************************************/
-    impl<'a> Builder<'a> {
-        pub fn new(module: &'a mut Module) -> Builder {
-            let context = module._context;
-            unsafe { Builder{ _module: module, _builder: rustc_llvm::LLVMCreateBuilderInContext(context) } }
-        }
-
-        pub fn get_int32_type(&mut self) -> Type { unsafe { Type{ _type: rustc_llvm::LLVMInt32TypeInContext(self._module._context) } } }
+        pub fn get_int32_type(&mut self) -> Type { unsafe { Type{ _type: rustc_llvm::LLVMInt32TypeInContext(self._context) } } }
 
         pub fn create_function_type(&mut self, return_type: &LLVMType, param_types: &[&LLVMType]) -> FunctionType {
             let llvm_types: Vec<rustc_llvm::TypeRef> = param_types.iter().map(|t| t.to_llvm_type()).collect();
@@ -114,14 +105,55 @@ mod llvm {
                 FunctionType{ _type: rustc_llvm::LLVMFunctionType(return_type.to_llvm_type(), p_types, param_types.len() as libc::c_uint, rustc_llvm::False) }
             }
         }
-        
-        pub fn create_function(&mut self, name: &str, func_type: &FunctionType) -> Function {
-            unsafe { Function{ _value: rustc_llvm::LLVMAddFunction(self._module._module, _get_string_ptr(name), func_type._type) } }
+
+        pub fn add_function(&mut self, name: &str, func_type: &FunctionType) -> Function {
+            unsafe { Function { 
+                _value: rustc_llvm::LLVMAddFunction(self._module, _get_string_ptr(name), func_type.to_llvm_type()),
+                _module: self
+            } }
         }
 
-        pub fn create_basic_block(&mut self, name: &str, func: &mut Function) -> BasicBlock {
-            unsafe { BasicBlock{ _block: rustc_llvm::LLVMAppendBasicBlockInContext(self._module._context, func._value, _get_string_ptr(name)) } }
+        pub fn print(&self) { unsafe { rustc_llvm::LLVMDumpModule(self._module); } }
+    }
+
+    /*************************************************************************/
+    impl Function {
+        pub fn create_basic_block(&mut self, name: &str) -> BasicBlock {
+            unsafe { BasicBlock{ 
+                _block: rustc_llvm::LLVMAppendBasicBlockInContext((*self._module)._context, self._value, _get_string_ptr(name)),
+                _function: self
+            } }
         }
+    }
+
+    /*************************************************************************/
+    impl BasicBlock {
+        pub fn build<F>(&mut self, builder_fn: F)
+            where F: Fn(&mut Builder) {
+            unsafe {
+                let mut builder = Builder::new((*self._function)._module);
+                builder.set_insert_point(self);
+                builder_fn(&mut builder);
+            }
+        }
+    }
+
+    /*************************************************************************/
+    impl Builder {
+        pub fn new(module: *mut Module) -> Builder {
+            unsafe {
+                let context = (*module)._context;
+                Builder{ _builder: rustc_llvm::LLVMCreateBuilderInContext(context) }
+            }
+        }
+
+        /*pub fn create_function(&mut self, name: &str, func_type: &FunctionType) -> Function {
+            unsafe { Function{ _value: rustc_llvm::LLVMAddFunction(self._module._module, _get_string_ptr(name), func_type._type) } }
+        }*/
+
+        /*pub fn create_basic_block(&mut self, name: &str, func: &mut Function) -> BasicBlock {
+            unsafe { BasicBlock{ _block: rustc_llvm::LLVMAppendBasicBlockInContext(self._module._context, func._value, _get_string_ptr(name)) } }
+        }*/
 
         pub fn set_insert_point(&mut self, block: &BasicBlock) {
             unsafe { rustc_llvm::LLVMPositionBuilderAtEnd(self._builder, block._block); }
@@ -169,25 +201,21 @@ mod llvm {
 
 /*************************************************************************/
 fn main() {
-    let mut module = llvm::Module::new("ModuloDePrueba");
-    let mut function: llvm::Function;
+    let mut module = llvm::Module::new("MainModule");
 
-    {
-        let mut builder = llvm::Builder::new(&mut module);
-        let return_type = builder.get_int32_type();
-        let param1_type = builder.get_int32_type();
-        let param2_type = builder.get_int32_type();
-        let function_type = builder.create_function_type(&return_type, &[&param1_type, &param2_type]);
-        function = builder.create_function("foo", &function_type);
-        let init_block = builder.create_basic_block("init", &mut function);
-        {
-            builder.set_insert_point(&init_block);
-            let param1 = builder.get_param(&function, 0);
-            let param2 = builder.get_param(&function, 1);
-            let sum = builder.create_add(&param1, &param2);
-            builder.create_ret(&sum);
-        }
-    }
+    let return_type = module.get_int32_type();
+    let param1_type = module.get_int32_type();
+    let param2_type = module.get_int32_type();
+    let function_type = module.create_function_type(&return_type, &[&param1_type, &param2_type]);
+    let mut function = module.add_function("foo", &function_type);
+    
+    function.create_basic_block("init").build(|builder| {
+        let param1 = builder.get_param(&function, 0);
+        let param2 = builder.get_param(&function, 1);
+        let sum = builder.create_add(&param1, &param2);
+        builder.create_ret(&sum);
+    });
+
     module.print();
 
     let mut engine = llvm::ExecutionEngine::new(module);
@@ -196,9 +224,9 @@ fn main() {
     assert!(!fptr.is_null());
     unsafe {
         println!("Function pointer: {:?}", fptr);
-        println!("Ejecutando...");
-        let f: fn(i32, i32) -> i32 = mem::transmute(fptr);
-        let res = f(12, 13);
+        println!("Running JITted function...");
+        let foo: fn(i32, i32) -> i32 = mem::transmute(fptr);
+        let res = foo(12, 13);
         println!("res: {}", res);
     }
 }
